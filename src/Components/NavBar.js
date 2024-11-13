30c91a87ec006505edc0c998b9e81186aa346bcd
Bruno A Parisi <bnoprs00@gmail.com>
2024-11-12 21:28:52 -0300

    Hooks y efectos visuales

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineMenu } from "react-icons/ai";
 import { IoMdClose } from "react-icons/io";
 import styles from './NavBar.module.css';
 import NavLinks from './NavLinks';
 
-
 const NavBar = () => {
     const [navBarOpen, setNavBarOpen] = useState(false);
+    
+    const [windowDimension, setWindowDimension] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    const detectDimension = () => {
+        setWindowDimension({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', detectDimension);
+
+        // Cierra el menú si la ventana es mayor a 800px de ancho
+        if (windowDimension.width > 800) {
+            setNavBarOpen(false);
+        }
+
+        return () => {
+            window.removeEventListener('resize', detectDimension);
+        };
+    }, [windowDimension]);
+    
     const links = [
         { id: 1, link: "Home" },
         { id: 2, link: "Services" },
@@ -23,17 +48,17 @@ const NavBar = () => {
                 {/* Mostrar el logo solo cuando el menú está cerrado */}
                 {!navBarOpen && <p>DISO | Digital Solutions</p>}
             </div>
-            {!navBarOpen ? (
+            {!navBarOpen && windowDimension.width < 800 ? (
                 <button className={styles.navToggle} onClick={toggleNavBar}>
                     <AiOutlineMenu size={25} />
                 </button>
-            ) : (
+            ) : windowDimension.width < 800 && (
                 <button className={styles.navToggle} onClick={toggleNavBar}>
                     <IoMdClose size={25} />
                 </button>
             )}
             {/* Mostrar NavLinks solo cuando el menú está abierto */}
-            {navBarOpen && <NavLinks links={links} onClick={toggleNavBar} />}
+            {(navBarOpen || windowDimension.width > 800) && <NavLinks links={links} onClick={toggleNavBar} />}
         </nav>
     );
 };
